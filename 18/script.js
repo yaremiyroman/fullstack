c60115b1950333deca0bdb183c9b5bf01b7769a6
andrdrx <yaremiyroman@gmail.com>
2026-05-20 17:29:16 +0300

    JS started

---
+++ 18/script.js
@@ -0,0 +1,248 @@
+// // alert('Hello, World!');
+
+// /*
+// console.log('Hello Hola Pryvit', 'Another string');
+// asdgf
+// asdf
+// asdfas
+// dfgs
+// dgs
+// dfgds
+// This is my 1st program
+// console.log(55);
+
+// */
+
+
+// alert('ASYNC');
+
+// console.log('Changes Hello Hola Pryvit', 'Another string');
+
+// console.log(2);
+// console.log(3);
+
+
+
+// console.log(4);
+
+// 5 + 7;
+
+// var a = 'hello';
+// console.log(4);
+// console.log(4);
+// console.log(4);
+
+
+// {
+//     var a = 'hello';
+//     console.log(4);
+//     console.log(4);
+//     console.log(4);
+// }
+
+// {
+
+// }
+
+// {
+//     var a = 'hello';
+// }
+
+
+
+// var sum = 10 + 5;
+// sum++;
+
+// sum > 10   ?  'hello' : 'world'
+
+
+// console.log(10);
+
+// console.log('hello');
+
+
+// let a = 10;
+
+// console.log(a);
+
+// let b;
+
+// console.log(b);
+
+// b = 111;
+// b = 0.1;
+
+// console.log(b);
+
+// let c = a + b;
+
+// console.log(c);
+
+
+// const x = 17;
+
+
+
+// console.log(x);
+
+// const currentYear = 2026;
+
+// const day = 'Wednesday';
+
+// const PI = 3.14;
+// const THIS_DAY = 'today';
+
+
+// camelCase
+// firstWordForVariable
+
+
+// const ukraineLang = 'українськаМова';
+
+// console.log(українськаМова);
+
+
+
+// const a = 10;
+// console.log(a);
+
+// const A = 11;
+// console.log(A);
+
+
+
+// const 43 = '123';
+
+// const #$%^&*(*&%$% ^&*) = '123';
+
+
+// const $1 = '1';
+// const $2 = '2';
+// const $3 = '3';
+
+
+
+// String: Текст.
+
+// 'Text'
+// const text = 'Lorem "NEW NEW " Ipsum';
+// const text2 = "Lorem 'Ipsum' 2";
+
+
+// const text3 = `Lorem 'BACKTICKS' 3`;
+
+// console.log('text > ', text);
+// console.log('text2 > ', text2);
+// console.log('text3 > ', text3);
+
+
+
+// const emptyText = '';
+// console.log('emptyText > ', emptyText);
+
+
+// const emptyText2 = ' ';
+// console.log('emptyText2 > ', emptyText2);
+
+// const emptyText3 = '                                  ';
+// console.log('emptyText3 > ', emptyText3);
+
+
+
+
+// Number: Числа.
+
+// int   integer
+// const a = 1
+// const b = -1;
+// const zero = 0;
+// const bigNumber = 100500;
+// const negative = -1001;
+
+// // float
+// const decimalExample = 0.42;
+// const decimalExample2 = 110.000042;
+
+// console.log('decimalExample2 > ', decimalExample2);
+
+
+
+// '234523453245234523452346234634623462346523462346'
+
+// BigInt: Великі цілі числа.
+// const veryBigInt = 243634263456345634563456345634563455634563456n + 1n;
+
+
+// console.log('veryBigInt > ', veryBigInt);
+
+
+
+// Boolean: true або false.
+
+// const first = true;
+// const anotherVar = false;
+
+
+
+// Null: Відсутність значення.
+
+// const someValue = 123;
+// const negativeValue = -4;
+// const zeroValue = 0;
+
+// let nullValue = null;
+// nullValue = 123;
+// nullValue = '123';
+// nullValue = false;
+// nullValue = null;
+
+
+
+
+
+
+let myNumber = 12;
+
+myNumber = 'string';
+
+
+
+
+
+// Undefined: Невизначене значення.
+
+
+// let a;
+
+// console.log('a > ', a);
+
+// const someVar = undefined;
+
+
+
+
+// // Array
+
+// const myArr = [3, 5, 6, 7];
+
+// // Object
+
+// const student = {
+//     name: 'Anon',
+//     secondName: 'Smith'
+// };
+
+// const cat = {
+//     name: 'Tom',
+//     breed: 'Sphynx',
+// }
+
+
+
+let num = 12345.6789;
+
+
+
+
+
+console.log(num.toFixed(2)); // '12345.68' - округлення до двох знаків після коми
