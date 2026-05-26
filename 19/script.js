ac7d0496800081f78f0563e9d4951bf773351f3f
andrdrx <yaremiyroman@gmail.com>
2026-05-26 19:17:12 +0300

    20 start

---
+++ 19/script.js
@@ -0,0 +1,347 @@
+// const x = 9;
+// const a = x + 10;
+
+// const stringExample = 'new string';
+// const boolExample = true;
+
+// const simpleObject = {};
+// const myArray = [];
+
+// console.log(typeof a);
+// console.log(typeof stringExample);
+// console.log(typeof boolExample);
+// console.log(typeof simpleObject);
+// console.log(typeof myArray);
+
+
+// const a = 9;
+
+// const b = -a;
+
+// console.log(b);
+
+
+// const a = '9';
+
+// const b = -a;
+
+// console.log(b);
+
+
+
+// const a = 'sdfgsdfgds91';
+
+// const b = -a;
+
+// console.log(b);
+
+
+// const a = '123';
+
+// const b = +a;
+
+// console.log(b);
+
+
+// increment
+// decrement
+// let a = 5;
+
+
+// a++; 
+// ++a; // 7 
+
+// const b = 10 + ++a;
+
+// console.log('a > ', a);
+
+// console.log('b > ', b);
+
+
+
+
+// var sum = 10 + 5; // 15
+// var difference = 10 - 5; // 5
+// var product = 10 * 5; // 50
+// var quotient = 10 / 5; // 2
+
+
+// const isEqual = (10 == 5); // false bool
+// const isNotEqual = (10 != 5); // true
+// const isGreater = (10 > 5); // true
+// const isLess = (10 < 5); // false
+// const isGreaterOrEqual = (10 >= 10); // false
+
+
+// let a = 5;
+
+// a = a + 4;
+// a += 3;
+
+// console.log(a);
+
+
+// const myVar = true;
+
+// const newVar = !true; // => false
+
+// console.log('newVar > ', newVar);
+
+
+// const maxNumber = Math.max(10, 20, 30, 40, 50);
+// console.log(maxNumber); // Виведе: 50
+
+// const minNumber = Math.min(10, 20, 30, 40, 50);
+// console.log(minNumber); // Виведе: 10
+
+
+
+// const myVar = Math.min();
+// console.log('myVar > ', myVar);
+
+
+// const roundedNumber = Math.round(4.7);
+// console.log(roundedNumber); // Виведе: 5
+
+// const floorNumber = Math.floor(4.7);
+// console.log(floorNumber); // Виведе: 4
+
+// var sqrtNumber = Math.sqrt(-4);
+// console.log(sqrtNumber); // Виведе: 4
+
+// var powerNumber = Math.pow(3, 3); // 2 * 2 * 2 * 2
+// console.log(powerNumber); // Виведе: 16
+
+
+// var randomNumber = Math.floor(Math.random() * 1000);
+
+// console.log(randomNumber); // Виведе випадкове число між 0 (включно) та 1 (не включно)
+
+// const randomNum = Math.random();
+// console.log('randomNum > ', randomNum);
+
+
+// 999
+
+
+// const greeting = 'Привіт, світе! ';
+// const secondPart = ' some words';
+
+// const unitedString = greeting + ' asd ' + '\n' +  'fg ' + secondPart + '   ';
+
+// console.log(unitedString); // Виведе: Привіт, світе!
+
+
+// ''
+// ""
+// ``
+
+
+
+
+// const myString = myAge + ` Some text ` + myName + ` another` + ' simple string' + " double quotes";
+
+
+// const strstr = `Some text ` + 44;
+
+// const myName = 'John';
+// const myAge = 44;
+
+// const myString =
+// `Some lorem ipsume ${myName}
+// dolor ${myAge} sit amet
+// text my ${3 + 8} text ${myAge * 2}`;
+
+
+// console.log(myString);
+
+
+// const greeting = 'Привіт, світе!';
+// const letter = greeting[2]; // Отримуємо дев'ятий символ з рядка greeting
+// console.log(letter); // Виведе: с
+
+
+// const stringLength = greeting.length;
+// console.log("stringLength > ", stringLength);
+
+
+// console.log('Some string here'.length);
+
+
+// var text = 'Привіт, світе!';
+// console.log(text.charAt(8)); // Виведе: с
+// console.log(text[8]); // Виведе: с
+
+
+// var text = 'Привіт, світе!';
+// const mySubstr = text.substring(3);
+
+// console.log(mySubstr); // Виведе: Привіт
+
+
+// var text = 'Привіт, світе!';
+// console.log(text.slice(-6)); // Виведе: світе
+
+
+
+// var text = 'Привіт, світе!';
+// console.log(text.substr(1, 4)); // Виведе: риві
+
+
+
+// var text = 'Привіт, світе!';
+// console.log(text.indexOf('ві')); // Виведе: 8
+
+
+// var text = 'Привіт, ві ві ві ві світе!';
+// console.log(text.lastIndexOf('ві')); // Виведе: 
+
+
+// var text = 'Привіт, світе!';
+// console.log(text.replace('dfhdsfhdf', 'asdgsdfgdsg')); // Виведе: Привіт, друже!
+
+
+// var text = 'Привіт, світе!';
+// console.log(text.toUpperCase()); // Виведе: ПРИВІТ, СВІТЕ!
+// console.log(text.toLowerCase()); // Виведе: привіт, світе!
+
+
+// var text = 'Привіт, світе!';
+// console.log(text.includes('світе')); // Виведе: true
+
+
+// false && false
+// false
+// false && true
+// false
+// true && false
+// false
+// true && true
+// true
+
+
+
+// false || false
+// false
+// false || true
+// true
+// true || false
+// true
+// true || true
+// true
+
+
+// !true
+// false
+// !false
+// true
+
+
+
+
+// let myUser = null;
+
+// if (myUser == null)
+// let myUser;
+
+// if (typeof myUser == 'undefined')
+//
+//
+//
+// SERVER REQUEST
+
+
+
+
+// const incomingUser = {
+//     name: 'Jonh',
+//     secondName: 'Doe'
+// };
+
+
+// myUser = incomingUser;
+
+
+// let temperature = null;
+
+// temperature = 0;
+
+
+// temperature = null;
+
+
+
+// Number
+
+// const a = 100;
+// const zero = 0;
+// const b = -123.54;
+
+// const x = NaN; // Not a Number
+// const positiveInf = Infinity;
+// const negativeInf = -Infinity;
+
+// console.log(typeof x);
+
+
+
+
+const a = 4 + 9;
+const b = 2 + 9;
+const c = a + 3;
+const d = 0 + b;
+
+// Define function
+// function sum(x, y) {
+//    const result = x + y;
+
+//    return result;
+// }
+
+
+// const mySum = sum(5, 7);
+
+
+// console.log('mySum > ', mySum);
+
+
+
+// function sayHello() {
+//     console.log('-------------------');
+//     console.log('Hello! Hello! Hola!');
+//     console.log('-------------------');
+//     console.log('\n');
+// }
+
+
+// sayHello();
+// sayHello();
+// sayHello();
+// sayHello();
+// sayHello();
+// sayHello();
+// sayHello();
+
+
+
+
+// function sayHello(name) {
+//     console.log('-------------------');
+//     console.log('Hello! ' + name + ' Hola!');
+//     console.log('-------------------');
+// }
+
+
+// sayHello('Garry');
+// sayHello('John');
+// sayHello('sdfgdsg');
+// sayHello(123);
+
+
+// function myPaw(basis, potential) {
+//     return basis**potential;
+// }
+
+
+// const result = myPaw(2, 4);
+// console.log('result > ', result);
