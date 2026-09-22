b139bd7d4c8c765f59763ebb06ad6b653092d5fd
andrdrx <yaremiyroman@gmail.com>
2026-09-22 19:59:31 +0300

    users store

--- final/src/store.js
+++ final/src/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import postsReducer from './slices/postsSlice';
+import usersReducer from './slices/usersSlice';
 
 export const store = configureStore({
     reducer: {
         posts: postsReducer,
+        users: usersReducer,
     },
 });
 
